cd4555f4c227ae396085a4f4e8d2e73c6fda35d4
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-11 17:08:15 +0530

    Completed the Navbar Component

---
+++ airbnb-experiences/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="public/airbnb-logo.png"></img>
+    </nav>
+  );
+}
+
+export default Navbar;
