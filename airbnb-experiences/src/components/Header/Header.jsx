a2d004f5a550dffa1c79c940ce5e9c4f859c7b51
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-11 18:23:27 +0530

    Completed the Header Component

--- airbnb-experiences/src/components/Header/Header.jsx
+++ airbnb-experiences/src/components/Header/Header.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import './Header.css';
 
 function Header(){
-    return(
-        <header>
-
-        </header>
-    )
+    return (
+      <header className="header-part">
+        <img src="public/photo-grid.png"></img>
+        <h1>Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </header>
+    );
 }
 
 export default Header;
